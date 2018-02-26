2d463ccfa5d8f1343fbd256afb4e068a29382605
dpepper <pepper.daniel@gmail.com>
2018-02-26 15:52:05 -0800

    bump version and update changelog

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@
 """
 
 __author__ = 'dpepper'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 import setuptools
 import unittest
--- sqlalchemy_lightening/__init__.py
+++ sqlalchemy_lightening/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'dpepper'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 
 from collections import Iterable
