b78620058e5ead7efa2f442774576bcd7d00e025
dpepper <pepper.daniel@gmail.com>
2019-04-05 13:33:40 -0700

    .

--- tests/models/__init__.py
+++ tests/models/__init__.py
@@ -3,32 +3,32 @@ import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import sessionmaker
 
 sys.path = [ os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')) ] + sys.path
 
 from sqlalchemy_lightening import LighteningBase
 
-Base = declarative_base()
 
-
-
-class Person(LighteningBase, Base):
+@as_declarative()
+class BaseModel(LighteningBase):
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
 
     def __repr__(self):
       return "%s(%s): %s" % (self.__class__.__name__, self.id, self.name)
 
 
+class Person(BaseModel):
+    name = Column(String(250), nullable=False)
+
 
 
 engine = create_engine('sqlite:///:memory:')
 session = sessionmaker(bind=engine)()
 
-Base.metadata.create_all(engine)
-Base.metadata.bind = engine
+BaseModel.metadata.create_all(engine)
+BaseModel.metadata.bind = engine
 
 LighteningBase.query_class = session.query
